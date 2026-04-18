35e79beb11b36e83edf729fabda9780f806623b2
Valeriia Vereshchynska <ver.valeriia@gmail.com>
2026-04-18 20:14:37 +0200

    lab-2: added lab-2 files and completed exercises

---
+++ lab-02/import_car_from.py
@@ -0,0 +1,6 @@
+from car import Car
+
+
+car = Car("Toyota", "Corolla", 2020)
+car.drive(150)
+print(car.describe())
---
+++ lab-02/import_car_star.py
@@ -0,0 +1,6 @@
+from car import *
+
+
+car = Car("Toyota", "Corolla", 2020)
+car.drive(150)
+print(car.describe())
